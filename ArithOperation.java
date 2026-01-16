87a45e47054b93b3d45ecc132d1238fabbac24fb
Nilam Hatkar <nilamhatkar0519@gmail.com>
2026-01-16 10:01:24 +0530

    Add files via upload

---
+++ ArithOperation.java
@@ -0,0 +1,22 @@
+public class ArithOperation {
+	public static void main(String[] args){
+		
+		int num1 = 19;
+		int num2 = 5;
+
+		int sum = num1 + num2;
+		int sub = num1 - num2;
+		int mul = num1 * num2;
+		int div = num1 / num2;
+		int mod = num1 % num2;
+
+		System.out.println("First number : " +num1);
+		System.out.println("Second number : " +num2);
+
+		System.out.println("Additoin is : " +sum);
+		System.out.println("Subtraction is : " +sub);
+		System.out.println("Multiplication is : " +mul);
+		System.out.println("Division is : " +div);
+		System.out.println("Module is : " +mod);
+}
+}
