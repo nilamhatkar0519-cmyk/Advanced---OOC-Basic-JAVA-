f03400f34175135aa02e6748ad938e02ce2059c8
Nilam Hatkar <nilamhatkar0519@gmail.com>
2026-01-16 10:12:04 +0530

    Add files via upload

---
+++ SwapOfNum.java
@@ -0,0 +1,15 @@
+public class SwapOfNum{
+	public static void main(String[] args){
+		
+		int num1 =10;
+		int num2 = 19;
+		System.out.println("Before swaping of two numbers :  num1 = "+num1+ ", num2 = "+num2);
+
+		int temp =num1;
+		num1 = num2;
+		num2 = temp;
+		System.out.println("After swaping of two numbers :  num1 = "+num1+ ", num2 = "+num2);
+
+}
+
+}
